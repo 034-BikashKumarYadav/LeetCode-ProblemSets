a500b59e16c5f9c3bbe8391a1224ee51431887e3
BIKASH KUMAR YADAV <93582708+034-BikashKumarYadav@users.noreply.github.com>
2025-02-28 13:42:55 +0530

    Time: 0 ms (100%), Space: 48.4 MB (57.57%) - LeetHub

---
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
@@ -0,0 +1,19 @@
+class Solution {
+    public TreeNode reverseOddLevels(TreeNode root) {
+        DFS(root.left, root.right, 0);
+        return root;
+    }
+
+    private void DFS(TreeNode leftChild, TreeNode rightChild, int level) {
+        if (leftChild == null || rightChild == null) {
+            return;
+        }
+        if (level % 2 == 0) {
+            int temp = leftChild.val;
+            leftChild.val = rightChild.val;
+            rightChild.val = temp;
+        }
+        DFS(leftChild.left, rightChild.right, level + 1);
+        DFS(leftChild.right, rightChild.left, level + 1);
+    }
+}
